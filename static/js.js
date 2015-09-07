1501f2582f78b10b609ae2c20ee72aad9fa4268f
Your Name <landmatrosen@googlemail.com>
2015-09-07 21:02:53 +0200

    run in theme material and persona

--- static/js.js
+++ static/js.js
@@ -1,29 +1,62 @@
  $('document').ready(function() {
-   
+   var html = "";
   
     $(document ).on("click", ".playit", function() {    
-         $(".playdemo").stop();
-         $("#audioplayerPlayer").remove();
-         var html = "";
-        html += '<div id="audioplayerPlayer">';
+        if ( $("#audioplayerPlayer").attr("id") === "audioplayerPlayer" ) {
+           $(".playdemo").stop();
+           $("#audioplayerPlayer").html("");
+        
+        } else {
+        
+           $( "body" ).append('<div id="audioplayerPlayer"></div>');
+        }
+         
+
+         
+          html = "";
+        
            html += '<span id="audioplayer_user"></span>';
-           html += '<span><audio class="playdemo inline" src="" preload="auto" controls loop autoplay></audio></span>';     
+           html += '<span><audio class="playdemo inline" src="" preload="auto" controls loop ></audio></span>';     
            html += '<a  id="linkToContent" class="pointer inline" ></a>';
            html += '<span id="linkToDownload" class="fa fa-cloud-download pointer t20w inline hover" rel="nofollow"></span>';
            html += '<span  class="closeAudioPlayer right fa fa-times t30w pointer hover" ></span>';
-        html += '</div>';
+       
 
-       $( "body" ).append(html);  
+
+       $( "#audioplayerPlayer" ).html(html);  
       
+    
            
         var src 	= $(this).data("link"),   // link from file
             name 	= $(this).data("name"),   // name from file
             topic = $(".topic-title").text(),  // topic title
-            user 	= $(this).parent().parent().parent().parent().children().html(),  // user Name and Link
+            //user  = $(this).closest('.poster-avatar').html();            
+           // user 	= $(this).parent().parent().parent().parent().children().html(),  // user Name and Link
             URL 	= $(location).attr('href'),  // Topic Link                 
             PHAT  = $(location).attr('pathname'); // Returns path only
-            
-            
+          
+
+          
+        
+          // theme material
+          if ( $( ".post-block" ).length ) {
+ 
+             var  user  = $(this).closest('div[class^="post-block"]').children().html();
+             console.log(  "theme => material" ); 
+          }
+          
+          // theme persona
+          if ( $( ".clearfix" ).length ) {
+ 
+             var  user  = $(this).closest('.clearfix').children().html();
+              console.log(  "theme => persona" );
+          }
+          
+           console.log(  "user code => " + user);
+             
+                    
+                    
+                    
         $(".playdemo").attr("src", src);        
         $("#audioplayer_user").html(user);         
         $("#linkToContent").attr("href", URL).text(topic); 
