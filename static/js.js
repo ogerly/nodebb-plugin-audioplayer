85c452d417509885a8367f41c6693c4af238656e
Your Name <landmatrosen@googlemail.com>
2015-09-07 22:52:58 +0200

    update for NodeBB v0.8.1. run in  Theme: Persona, Material, Lavender, Vanilla

--- static/js.js
+++ static/js.js
@@ -16,7 +16,7 @@
           html = "";
         
            html += '<span id="audioplayer_user"></span>';
-           html += '<span><audio class="playdemo inline" src="" preload="auto" controls loop ></audio></span>';     
+           html += '<span><audio class="playdemo inline" src="" preload="auto" controls loop autoplay></audio></span>';     
            html += '<a  id="linkToContent" class="pointer inline" ></a>';
            html += '<span id="linkToDownload" class="fa fa-cloud-download pointer t20w inline hover" rel="nofollow"></span>';
            html += '<span  class="closeAudioPlayer right fa fa-times t30w pointer hover" ></span>';
@@ -30,8 +30,6 @@
         var src 	= $(this).data("link"),   // link from file
             name 	= $(this).data("name"),   // name from file
             topic = $(".topic-title").text(),  // topic title
-            //user  = $(this).closest('.poster-avatar').html();            
-           // user 	= $(this).parent().parent().parent().parent().children().html(),  // user Name and Link
             URL 	= $(location).attr('href'),  // Topic Link                 
             PHAT  = $(location).attr('pathname'); // Returns path only
           
@@ -39,17 +37,17 @@
           
         
           // theme material
-          if ( $( ".post-block" ).length ) {
+          if ( $( ".poster-avatar" ).length ) {
  
              var  user  = $(this).closest('div[class^="post-block"]').children().html();
-             console.log(  "theme => material" ); 
+            // console.log(  "theme => material" ); 
           }
           
           // theme persona
           if ( $( ".clearfix" ).length ) {
  
              var  user  = $(this).closest('.clearfix').children().children().html();
-              console.log(  "theme => persona" );
+            //  console.log(  "theme => persona" );
           }
           
           // theme lavender
@@ -58,23 +56,22 @@
              var  user  = $(this).closest('.col-md-12').children().children().html();
                   user  = "<a href='/user/"+ $(user).attr("alt") +"'>" + user + "</a>";
              
-              console.log(  "theme => lavender" );
+            //  console.log(  "theme => lavender" );
           }
           
           // theme vanilla
           if ( $( ".avatar" ).length ) {
  
-             var  user  = $(this).closest('.topic-item').html();
-
+             var  user  	= $(this).closest('.topic-item').children().html(),
+                  userURL  = $(this).closest('.topic-item').children().attr('href');
              
+             user  = "<a href='"+ userURL +"'>" + user + "</a>";
               console.log(  "theme => vanilla" );
           }
           
           
-           console.log(  "user code => " + user);
-             
-                    
-                    
+          // console.log(  "user code => " + user);
+                       
                     
         $(".playdemo").attr("src", src);        
         $("#audioplayer_user").html(user);         
@@ -90,7 +87,6 @@
    
    // close and remove audioplayer
       $(document ).on("click", ".closeAudioPlayer", function() {    
- 
          $(".playdemo").stop();
          $("#audioplayerPlayer").remove();
      });  
